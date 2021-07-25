ec9bf9e6f776f2b55c30cfde9a72f19dc80c8905
Ivana Matijevic <ivmatij@gmail.com>
2021-07-24 23:02:51 -0400

    adds basic geo search functionality

--- pages/search.vue
+++ pages/search.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>--- {{ $route.query }}</div>
+    <div>
+        <p>{{ lat }} / {{ lng }} / {{ label }}</p>
+        <div v-for="home in homes" :key="home.objectID">
+            {{ home.title }}
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    watchQuery: ["lat"],
+    async asyncData({ query, $dataApi }) {
+        const data = await $dataApi.getHomesByLocation(query.lat, query.lng);
+        return {
+            label: query.label,
+            lat: query.lat,
+            lng: query.lng,
+            homes: data.json.hits,
+        };
+    },
+};
+</script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -11,6 +11,7 @@ export default function(context, inject) {
         getHome,
         getReviewsByHomeId,
         getUserByHomeId,
+        getHomesByLocation,
     });
 
     async function getHome(homeId) {
@@ -60,6 +61,30 @@ export default function(context, inject) {
                         method: "POST",
                         body: JSON.stringify({
                             filters: `homeId:${homeId}`,
+                            attributesToHighlight: [],
+                        }),
+                    }
+                )
+            );
+        } catch (error) {
+            return getErrorResponse(error);
+        }
+    }
+
+    async function getHomesByLocation(lat, lng, radiusInMeters = 1500) {
+        try {
+            return unwrap(
+                await fetch(
+                    `https://${appId}-dsn.algolia.net/1/indexes/homes/query`,
+                    {
+                        headers,
+                        method: "POST",
+                        body: JSON.stringify({
+                            aroundLatLng: `${lat},${lng}`,
+                            // aroundRadius limits results to radius passed in
+                            // so if radiusInMeters = 1500 we will only see results 1500m from the search
+                            aroundRadius: radiusInMeters,
+                            hitsPerPage: 10,
                             attributesToHighlight: [],
                         }),
                     }

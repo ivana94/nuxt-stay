b2b01c7bd1711b84c5e424c090fec5ceed6f6820
Ivana Matijevic <ivmatij@gmail.com>
2021-11-19 15:07:21 -0500

    fixes footer styling bug

--- .nuxt/dist/api/ipx.js
+++
@@ -1,3 +0,0 @@
-import {createIPX, createIPXMiddleware} from "ipx";
-const ipx = createIPX({"dir":"/Users/ivanamatijevic/dev/nuxtbnb/static","domains":[],"sharp":{},"alias":{}});
-export default createIPXMiddleware(ipx);
